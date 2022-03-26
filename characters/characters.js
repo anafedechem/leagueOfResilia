4dae414b414f4c97edae46b886de05082104c13a
anafedechem <anafedechem@gmail.com>
2022-03-25 22:21:09 -0300

    configurei media queries para o jogo todo

--- characters/characters.js
+++ characters/characters.js
@@ -1,3 +1,8 @@
+//Função para deixar no canto inferior esquerdo das fases o personagem escolhido pelo pelo jogador
+//O primeiro herói jogará as fases 1, 2 e 3;
+//O segundo herói jogará as fases 4, 5 e 6;
+//O terceiro herói jogará as fases 7, 8 e 9.
+
 function avancar(){
     let selectedUm = document.getElementById("primeiro").value
     let selectedDois = document.getElementById("segundo").value
@@ -6,6 +11,7 @@ function avancar(){
     if (selectedUm == "0" || selectedDois == "0" || selectedTres == "0"){
         alert("Atenção! Você deve selecionar um personagem em cada campo")
         return
+        //"0" é a posição do <select> que tem o texto "escolha um personagem"
     }
     localStorage.setItem("primeiro", selectedUm);
     localStorage.setItem("segundo", selectedDois);
